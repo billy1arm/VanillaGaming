c2d2c794878361447f1448b0744f825061e61a45
xfury <xfury@foxmail.com>
2016-06-08 21:40:41 +0800

    [V-Core]修复技能-冲锋
    缩短距离

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -4588,7 +4588,7 @@ void Spell::EffectCharge(SpellEffectIndex /*eff_idx*/)
     // TODO: research more ContactPoint/attack distance.
     // 3.666666 instead of ATTACK_DISTANCE(5.0f) in below seem to give more accurate result.
     float x, y, z;
-    unitTarget->GetContactPoint(m_caster, x, y, z, 3.666666f);
+    unitTarget->GetContactPoint(m_caster, x, y, z, 1.5f);
 
     if (unitTarget->GetTypeId() != TYPEID_PLAYER)
         ((Creature*)unitTarget)->StopMoving();
