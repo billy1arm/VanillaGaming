01ab7e241d5825c59e8408c3f7760bb7ba3d693c
xfury <xfury@foxmail.com>
2016-06-09 02:29:31 +0800

    [V-Data]修复生物-联盟医师
    无法被攻击

---
+++ sql/update/creature_template.sql
@@ -0,0 +1,16 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : VanillaGaming
+Source Database       : z-mangos
+
+Target Server Type    : MYSQL
+File Encoding         : 65001
+*/
+
+-- ----------------------------
+-- Updates of creature_template
+-- ----------------------------
+
+-- 联盟医师
+UPDATE `creature_template` SET `UnitFlags`='2' WHERE (`Entry`='13116');
