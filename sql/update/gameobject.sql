157dff46cfb827b330e7285c83c68358a73a7032
xfury <xfury@foxmail.com>
2016-06-09 14:39:43 +0800

    [V-Data]调整道具-坚固的箱子
    刷新时间调整为2小时

---
+++ sql/update/gameobject.sql
@@ -0,0 +1,28 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : VanillaGaming
+Source Database       : z-mangos
+
+Target Server Type    : MYSQL
+File Encoding         : 65001
+*/
+
+-- ----------------------------
+-- Updates of gameobject
+-- ----------------------------
+
+-- 坚固的箱子
+UPDATE `gameobject` SET `spawntimesecs`='7200' WHERE (`id`='2850');
+
+-- 坚固的箱子
+UPDATE `gameobject` SET `spawntimesecs`='7200' WHERE (`id`='2852');
+
+-- 坚固的箱子
+UPDATE `gameobject` SET `spawntimesecs`='7200' WHERE (`id`='2855');
+
+-- 坚固的箱子
+UPDATE `gameobject` SET `spawntimesecs`='7200' WHERE (`id`='2857');
+
+-- 坚固的箱子
+UPDATE `gameobject` SET `spawntimesecs`='7200' WHERE (`id`='4149');
