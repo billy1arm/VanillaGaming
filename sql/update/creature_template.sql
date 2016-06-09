6043416841fb459a6f6fcd56ff5ba71d04f7db72
xfury <xfury@foxmail.com>
2016-06-09 15:08:10 +0800

    [V-Data]调整生物-祖尔法拉克阵亡英雄
    无经验

--- sql/update/creature_template.sql
+++ sql/update/creature_template.sql
@@ -12,6 +12,9 @@ File Encoding         : 65001
 -- Updates of creature_template
 -- ----------------------------
 
+-- 祖尔法拉克阵亡英雄
+UPDATE `creature_template` SET `ExtraFlags`='64' WHERE (`Entry`='7276');
+
 -- 联盟医师
 UPDATE `creature_template` SET `UnitFlags`='2' WHERE (`Entry`='13116');
 
