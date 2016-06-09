240679fd6bda88749c418e706832de378ceb4591
xfury <xfury@foxmail.com>
2016-06-10 02:07:08 +0800

    [V-Data]关闭部分副本
    1:奥妮克希亚的巢穴
    2:熔火之心
    3:黑翼之巢
    4:祖尔格拉布
    5:安其拉废墟
    6:安其拉神庙
    7:纳克萨玛斯

---
+++ sql/update/areatrigger_teleport.sql
@@ -0,0 +1,37 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : VanillaGaming
+Source Database       : z-mangos
+
+Target Server Type    : MYSQL
+File Encoding         : 65001
+*/
+
+-- ----------------------------
+-- Updates of areatrigger_teleport
+-- ----------------------------
+
+-- 奥妮克希亚的巢穴
+UPDATE `areatrigger_teleport` SET `required_level`='63' WHERE (`id`='2848');
+
+-- 熔火之心
+UPDATE `areatrigger_teleport` SET `required_level`='63' WHERE (`id`='2886');
+UPDATE `areatrigger_teleport` SET `required_level`='63' WHERE (`id`='3528');
+UPDATE `areatrigger_teleport` SET `required_level`='63' WHERE (`id`='3529');
+
+-- 黑翼之巢
+UPDATE `areatrigger_teleport` SET `required_level`='63' WHERE (`id`='3726');
+
+-- 祖尔格拉布
+UPDATE `areatrigger_teleport` SET `required_level`='63' WHERE (`id`='3928');
+
+-- 安其拉废墟
+UPDATE `areatrigger_teleport` SET `required_level`='63' WHERE (`id`='4008');
+
+-- 安其拉神庙
+UPDATE `areatrigger_teleport` SET `required_level`='63' WHERE (`id`='4010');
+
+-- 纳克萨玛斯
+UPDATE `areatrigger_teleport` SET `required_level`='63' WHERE (`id`='4055');
+UPDATE `areatrigger_teleport` SET `required_level`='63' WHERE (`id`='4156');
