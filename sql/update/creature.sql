2ce174b1ec829c445b1e6fc08e24663bcf9e4f25
xfury <xfury@foxmail.com>
2016-06-09 21:16:20 +0800

    [V-Data]调整生物-断牙
    刷新时间调整为2.8小时

--- sql/update/creature.sql
+++ sql/update/creature.sql
@@ -14,3 +14,6 @@ File Encoding         : 65001
 
 -- 鲁伯斯
 UPDATE `creature` SET `spawntimesecs`='18000' WHERE (`id`='521');
+
+-- 断牙
+UPDATE `creature` SET `spawntimesecs`='10200' WHERE (`id`='2850');
