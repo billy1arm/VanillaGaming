51aa814d598b4a5e1d0eefdeffa6778fff31a148
xfury <xfury@foxmail.com>
2016-06-09 20:44:04 +0800

    [V-Data]调整道具-补给箱
    刷新时间调整为12小时

--- sql/update/gameobject.sql
+++ sql/update/gameobject.sql
@@ -41,3 +41,21 @@ UPDATE `gameobject` SET `spawntimesecs`='43200' WHERE (`id`='75300');
 
 -- 坚固的大箱子
 UPDATE `gameobject` SET `spawntimesecs`='43200' WHERE (`id`='153464');
+
+-- 补给箱
+UPDATE `gameobject` SET `spawntimesecs`='43200' WHERE (`id`='166872');
+
+-- 补给箱
+UPDATE `gameobject` SET `spawntimesecs`='43200' WHERE (`id`='176224');
+
+-- 补给箱
+UPDATE `gameobject` SET `spawntimesecs`='43200' WHERE (`id`='176304');
+
+-- 补给箱
+UPDATE `gameobject` SET `spawntimesecs`='43200' WHERE (`id`='176307');
+
+-- 补给箱
+UPDATE `gameobject` SET `spawntimesecs`='43200' WHERE (`id`='176308');
+
+-- 补给箱
+UPDATE `gameobject` SET `spawntimesecs`='43200' WHERE (`id`='176309');
