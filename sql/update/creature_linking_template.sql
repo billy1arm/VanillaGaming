bf269ab1b66bfe98e08438f655e73cd7cead901a
xfury <xfury@foxmail.com>
2016-06-09 17:07:15 +0800

    [V-Data]修复生物-多彩精英卫士
    仇恨连接

---
+++ sql/update/creature_linking_template.sql
@@ -0,0 +1,16 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : VanillaGaming
+Source Database       : z-mangos
+
+Target Server Type    : MYSQL
+File Encoding         : 65001
+*/
+
+-- ----------------------------
+-- Updates of creature_linking_template
+-- ----------------------------
+
+-- 多彩精英卫士
+UPDATE `creature_linking_template` SET `flag`='135' WHERE (`entry`='10814') AND (`map`='229');
