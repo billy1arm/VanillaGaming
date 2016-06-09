23ac9a136c297617f31279b6e5653c0365c51c4e
xfury <xfury@foxmail.com>
2016-06-09 21:03:35 +0800

    [V-Data]调整生物-鲁伯斯
    刷新时间调整为5小时

---
+++ sql/update/creature.sql
@@ -0,0 +1,16 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : VanillaGaming
+Source Database       : z-mangos
+
+Target Server Type    : MYSQL
+File Encoding         : 65001
+*/
+
+-- ----------------------------
+-- Updates of creature
+-- ----------------------------
+
+-- 鲁伯斯
+UPDATE `creature` SET `spawntimesecs`='18000' WHERE (`id`='521');
