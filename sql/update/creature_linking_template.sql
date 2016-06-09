47082086f3f420f238d8b99c7b4c1a318189f869
xfury <xfury@foxmail.com>
2016-06-09 17:11:56 +0800

    [V-Data]修复生物-观察者克鲁什
    仇恨连接

--- sql/update/creature_linking_template.sql
+++ sql/update/creature_linking_template.sql
@@ -14,3 +14,6 @@ File Encoding         : 65001
 
 -- 多彩精英卫士
 UPDATE `creature_linking_template` SET `flag`='135' WHERE (`entry`='10814') AND (`map`='229');
+	
+-- 观察者克鲁什
+UPDATE `creature_linking_template` SET `flag`='135' WHERE (`entry`='14324') AND (`map`='429');
