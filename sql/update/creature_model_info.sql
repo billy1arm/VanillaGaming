44c97f5b9182daae0f85a319003ee8221853bb53
xfury <xfury@foxmail.com>
2016-06-10 22:47:17 +0800

    [V-Data]修复生物-苍白圣殿骑士
    1:模型大小
    2:新增数据库AI

---
+++ sql/update/creature_model_info.sql
@@ -0,0 +1,16 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : VanillaGaming
+Source Database       : z-mangos
+
+Target Server Type    : MYSQL
+File Encoding         : 65001
+*/
+
+-- ----------------------------
+-- Updates of creature_model_info
+-- ----------------------------
+
+-- 苍白圣殿骑士
+UPDATE `creature_model_info` SET `bounding_radius`='1', `combat_reach`='1' WHERE (`modelid`='14502');
