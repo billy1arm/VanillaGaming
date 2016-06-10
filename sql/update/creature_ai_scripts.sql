787dc74475e512c67369bba8d92866c307230ed8
xfury <xfury@foxmail.com>
2016-06-11 04:41:27 +0800

    [V-Data]修复生物-狂怒傀儡
    数据库AI-施放-乱舞

--- sql/update/creature_ai_scripts.sql
+++ sql/update/creature_ai_scripts.sql
@@ -15,6 +15,9 @@ File Encoding         : 65001
 -- 奈塔拉什
 DELETE FROM `creature_ai_scripts` WHERE `id`='198302';
 
+-- 狂怒傀儡
+UPDATE `creature_ai_scripts` SET `action1_param2`='0', `comment`='狂怒傀儡 - 施放-乱舞' WHERE (`id`='890602');
+
 -- 暮光地王
 DELETE FROM `creature_ai_scripts` WHERE `creature_id`='11881';
 INSERT INTO `creature_ai_scripts` (`id`, `creature_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_type`, `action1_param1`, `action1_param2`, `action1_param3`, `action2_type`, `action2_param1`, `action2_param2`, `action2_param3`, `action3_type`, `action3_param1`, `action3_param2`, `action3_param3`, `comment`) VALUES ('1188101', '11881', '9', '0', '100', '1', '5', '30', '3000', '3000', '11', '9483', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '暮光地王 - 施放-投石');
