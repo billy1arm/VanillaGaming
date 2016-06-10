705415467c34857c822a416807433cb19ab11d07
xfury <xfury@foxmail.com>
2016-06-11 01:01:14 +0800

    [V-Data]修复技能-毒蝎钉刺
    训练师处正确需要按照技能等级学习

--- sql/update/spell_chain.sql
+++ sql/update/spell_chain.sql
@@ -12,5 +12,12 @@ File Encoding         : 65001
 -- Updates of spell_chain
 -- ----------------------------
 
+-- 毒蝎钉刺
+DELETE FROM `spell_chain` WHERE `first_spell`='3043';
+INSERT INTO `spell_chain` (`spell_id`, `prev_spell`, `first_spell`, `rank`, `req_spell`) VALUES ('3043', '0', '3043', '1', '0');
+INSERT INTO `spell_chain` (`spell_id`, `prev_spell`, `first_spell`, `rank`, `req_spell`) VALUES ('14275', '3043', '3043', '2', '0');
+INSERT INTO `spell_chain` (`spell_id`, `prev_spell`, `first_spell`, `rank`, `req_spell`) VALUES ('14276', '14275', '3043', '3', '0');
+INSERT INTO `spell_chain` (`spell_id`, `prev_spell`, `first_spell`, `rank`, `req_spell`) VALUES ('14277', '14276', '3043', '4', '0');
+
 -- 强效光明祝福
 UPDATE `spell_chain` SET `prev_spell`='19979', `first_spell`='19977', `rank`='4', `req_spell`='0' WHERE (`spell_id`='25890');
