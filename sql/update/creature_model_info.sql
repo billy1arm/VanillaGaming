cb12a2b689505f6105aaccabd82f85b3f4e5c717
xfury <xfury@foxmail.com>
2016-06-10 22:47:55 +0800

    [V-Data]修复生物-赤红圣殿骑士
    模型大小

--- sql/update/creature_model_info.sql
+++ sql/update/creature_model_info.sql
@@ -12,5 +12,8 @@ File Encoding         : 65001
 -- Updates of creature_model_info
 -- ----------------------------
 
+-- 赤红圣殿骑士
+UPDATE `creature_model_info` SET `bounding_radius`='1', `combat_reach`='1' WHERE (`modelid`='1204');
+
 -- 苍白圣殿骑士
 UPDATE `creature_model_info` SET `bounding_radius`='1', `combat_reach`='1' WHERE (`modelid`='14502');
