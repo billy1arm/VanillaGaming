59db2ec8bb003cbe342736e85e25afb93743db99
xfury <xfury@foxmail.com>
2016-06-11 20:38:29 +0800

    [V-Data]调整任务-希兹尔的飞行器
    1:精细的双帆龙鳞片-掉率
    2:精细的翼手龙鳞片-掉率

--- sql/update/creature_loot_template.sql
+++ sql/update/creature_loot_template.sql
@@ -15,3 +15,21 @@ File Encoding         : 65001
 -- 堕落的水之魂 --- 剩余的最纯净的水
 DELETE FROM `creature_loot_template` WHERE (`entry`='5894') AND (`item`='7811');
 INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES ('5894', '7811', '-100', '0', '1', '1', '0');
+
+-- 幼双帆龙 --- 精细的双帆龙鳞片
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance`='-80' WHERE (`entry`='9162') AND (`item`='11830');
+
+-- 双帆龙 --- 精细的双帆龙鳞片
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance`='-80' WHERE (`entry`='9163') AND (`item`='11830');
+
+-- 老双帆龙 --- 精细的双帆龙鳞片
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance`='-80' WHERE (`entry`='9164') AND (`item`='11830');
+
+-- 小翼手龙 --- 精细的翼手龙鳞片
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance`='-80' WHERE (`entry`='9165') AND (`item`='11831');
+
+-- 翼手龙 --- 精细的翼手龙鳞片
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance`='-80' WHERE (`entry`='9166') AND (`item`='11831');
+
+-- 狂怒的翼手龙 --- 精细的翼手龙鳞片
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance`='-80' WHERE (`entry`='9167') AND (`item`='11831');
