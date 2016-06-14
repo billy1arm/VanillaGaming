0f6320d9fcb90c5084882c8a47aff084315723c5
xfury <xfury@foxmail.com>
2016-06-14 16:17:35 +0800

    [V-Locales]更新汉化
    雷霆崖守卫

---
+++ sql/update/gossip_menu_option.sql
@@ -0,0 +1,32 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : VanillaGaming
+Source Database       : z-mangos
+
+Target Server Type    : MYSQL
+File Encoding         : 65001
+*/
+
+-- ----------------------------
+-- Updates of gossip_menu_option
+-- ----------------------------
+
+-- 721
+UPDATE `gossip_menu_option` SET `option_text`='银行' WHERE (`menu_id`='721') AND (`id`='0');
+UPDATE `gossip_menu_option` SET `option_text`='双足飞龙管理员' WHERE (`menu_id`='721') AND (`id`='1');
+UPDATE `gossip_menu_option` SET `option_text`='公会管理员' WHERE (`menu_id`='721') AND (`id`='2');
+UPDATE `gossip_menu_option` SET `option_text`='旅店' WHERE (`menu_id`='721') AND (`id`='3');
+UPDATE `gossip_menu_option` SET `option_text`='邮箱' WHERE (`menu_id`='721') AND (`id`='4');
+UPDATE `gossip_menu_option` SET `option_text`='拍卖行' WHERE (`menu_id`='721') AND (`id`='5');
+UPDATE `gossip_menu_option` SET `option_text`='武器大师' WHERE (`menu_id`='721') AND (`id`='6');
+UPDATE `gossip_menu_option` SET `option_text`='兽栏管理员' WHERE (`menu_id`='721') AND (`id`='7');
+UPDATE `gossip_menu_option` SET `option_text`='战场军官' WHERE (`menu_id`='721') AND (`id`='8');
+UPDATE `gossip_menu_option` SET `option_text`='职业训练师' WHERE (`menu_id`='721') AND (`id`='9');
+UPDATE `gossip_menu_option` SET `option_text`='专业训练师' WHERE (`menu_id`='721') AND (`id`='10');
+
+-- 751
+UPDATE `gossip_menu_option` SET `option_text`='制皮' WHERE (`menu_id`='751') AND (`id`='8');
+UPDATE `gossip_menu_option` SET `option_text`='采矿' WHERE (`menu_id`='751') AND (`id`='9');
+UPDATE `gossip_menu_option` SET `option_text`='剥皮' WHERE (`menu_id`='751') AND (`id`='10');
+UPDATE `gossip_menu_option` SET `option_text`='裁缝' WHERE (`menu_id`='751') AND (`id`='11');
