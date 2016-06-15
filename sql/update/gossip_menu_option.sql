ad3dd55c24fb533b5a5ce3938d6c842fb0817245
xfury <xfury@foxmail.com>
2016-06-15 17:26:49 +0800

    [V-Locales]更新汉化
    暴风城卫兵

--- sql/update/gossip_menu_option.sql
+++ sql/update/gossip_menu_option.sql
@@ -12,6 +12,20 @@ File Encoding         : 65001
 -- Updates of gossip_menu_option
 -- ----------------------------
 
+-- 435
+UPDATE `gossip_menu_option` SET `option_text`='拍卖行' WHERE (`menu_id`='435') AND (`id`='0');
+UPDATE `gossip_menu_option` SET `option_text`='暴风城银行' WHERE (`menu_id`='435') AND (`id`='1');
+UPDATE `gossip_menu_option` SET `option_text`='矿道地铁' WHERE (`menu_id`='435') AND (`id`='3');
+UPDATE `gossip_menu_option` SET `option_text`='旅馆' WHERE (`menu_id`='435') AND (`id`='4');
+UPDATE `gossip_menu_option` SET `option_text`='狮鹫管理员' WHERE (`menu_id`='435') AND (`id`='5');
+UPDATE `gossip_menu_option` SET `option_text`='公会管理员' WHERE (`menu_id`='435') AND (`id`='6');
+UPDATE `gossip_menu_option` SET `option_text`='兽栏管理员' WHERE (`menu_id`='435') AND (`id`='8');
+UPDATE `gossip_menu_option` SET `option_text`='武器训练师' WHERE (`menu_id`='435') AND (`id`='9');
+UPDATE `gossip_menu_option` SET `option_text`='军官休息室' WHERE (`menu_id`='435') AND (`id`='10');
+UPDATE `gossip_menu_option` SET `option_text`='战场军官' WHERE (`menu_id`='435') AND (`id`='11');
+UPDATE `gossip_menu_option` SET `option_text`='职业训练师' WHERE (`menu_id`='435') AND (`id`='14');
+UPDATE `gossip_menu_option` SET `option_text`='专业训练师' WHERE (`menu_id`='435') AND (`id`='15');
+
 -- 721
 UPDATE `gossip_menu_option` SET `option_text`='银行' WHERE (`menu_id`='721') AND (`id`='0');
 UPDATE `gossip_menu_option` SET `option_text`='双足飞龙管理员' WHERE (`menu_id`='721') AND (`id`='1');
