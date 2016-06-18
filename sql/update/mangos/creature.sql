9bf0f0809e6b2b9ae956691230ee35ab4ba8f6f5
xfury <xfury@foxmail.com>
2016-06-18 23:19:52 +0800

    [V-Data]修复生物-蛮锤哨兵
    路径

--- sql/update/mangos/creature.sql
+++ sql/update/mangos/creature.sql
@@ -54,3 +54,6 @@ DELETE FROM `creature` WHERE `id`='14889';
 -- 泰拉尔
 DELETE FROM `creature` WHERE `id`='14890';
 -- INSERT INTO `creature` (`guid`, `id`, `map`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `DeathState`, `MovementType`) VALUES ('4256', '14890', '0', '15363', '0', '-10428.8', '-392.176', '43.7411', '0.932375', '604800', '5', '0', '832750', '8640', '0', '2');
+
+-- 蛮锤哨兵
+UPDATE `creature` SET `position_x`='242.109', `position_y`='-2175.42', `position_z`='118.703', `orientation`='5.06145', `spawndist`='0', `MovementType`='2' WHERE (`guid`='92919');
---
+++ sql/update/mangos/creature_movement.sql
@@ -0,0 +1,34 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : VanillaGaming
+Source Database       : z-mangos
+
+Target Server Type    : MYSQL
+File Encoding         : 65001
+*/
+
+-- ----------------------------
+-- Updates of creature_movement
+-- ----------------------------
+
+-- 蛮锤哨兵
+DELETE FROM `creature_movement` WHERE (`id`='92919');
+INSERT INTO `creature_movement` (`id`, `point`, `position_x`, `position_y`, `position_z`, `waittime`, `script_id`, `textid1`, `textid2`, `textid3`, `textid4`, `textid5`, `emote`, `spell`, `orientation`, `model1`, `model2`) VALUES ('92919', '1', '233.113', '-2162.84', '120.015', '0', '0', '0', '0', '0', '0', '0', '0', '0', '100', '0', '0');
+INSERT INTO `creature_movement` (`id`, `point`, `position_x`, `position_y`, `position_z`, `waittime`, `script_id`, `textid1`, `textid2`, `textid3`, `textid4`, `textid5`, `emote`, `spell`, `orientation`, `model1`, `model2`) VALUES ('92919', '2', '233.087', '-2143.76', '119.295', '0', '0', '0', '0', '0', '0', '0', '0', '0', '100', '0', '0');
+INSERT INTO `creature_movement` (`id`, `point`, `position_x`, `position_y`, `position_z`, `waittime`, `script_id`, `textid1`, `textid2`, `textid3`, `textid4`, `textid5`, `emote`, `spell`, `orientation`, `model1`, `model2`) VALUES ('92919', '3', '230.995', '-2125.5', '117.604', '0', '0', '0', '0', '0', '0', '0', '0', '0', '100', '0', '0');
+INSERT INTO `creature_movement` (`id`, `point`, `position_x`, `position_y`, `position_z`, `waittime`, `script_id`, `textid1`, `textid2`, `textid3`, `textid4`, `textid5`, `emote`, `spell`, `orientation`, `model1`, `model2`) VALUES ('92919', '4', '233.908', '-2110.25', '117.959', '0', '0', '0', '0', '0', '0', '0', '0', '0', '100', '0', '0');
+INSERT INTO `creature_movement` (`id`, `point`, `position_x`, `position_y`, `position_z`, `waittime`, `script_id`, `textid1`, `textid2`, `textid3`, `textid4`, `textid5`, `emote`, `spell`, `orientation`, `model1`, `model2`) VALUES ('92919', '5', '235.483', '-2086.11', '122.593', '0', '0', '0', '0', '0', '0', '0', '0', '0', '100', '0', '0');
+INSERT INTO `creature_movement` (`id`, `point`, `position_x`, `position_y`, `position_z`, `waittime`, `script_id`, `textid1`, `textid2`, `textid3`, `textid4`, `textid5`, `emote`, `spell`, `orientation`, `model1`, `model2`) VALUES ('92919', '6', '224.699', '-2066.14', '127.224', '0', '0', '0', '0', '0', '0', '0', '0', '0', '100', '0', '0');
+INSERT INTO `creature_movement` (`id`, `point`, `position_x`, `position_y`, `position_z`, `waittime`, `script_id`, `textid1`, `textid2`, `textid3`, `textid4`, `textid5`, `emote`, `spell`, `orientation`, `model1`, `model2`) VALUES ('92919', '7', '207.454', '-2045.2', '133.071', '0', '0', '0', '0', '0', '0', '0', '0', '0', '100', '0', '0');
+INSERT INTO `creature_movement` (`id`, `point`, `position_x`, `position_y`, `position_z`, `waittime`, `script_id`, `textid1`, `textid2`, `textid3`, `textid4`, `textid5`, `emote`, `spell`, `orientation`, `model1`, `model2`) VALUES ('92919', '8', '198.622', '-2032.04', '137.491', '0', '0', '0', '0', '0', '0', '0', '0', '0', '100', '0', '0');
+INSERT INTO `creature_movement` (`id`, `point`, `position_x`, `position_y`, `position_z`, `waittime`, `script_id`, `textid1`, `textid2`, `textid3`, `textid4`, `textid5`, `emote`, `spell`, `orientation`, `model1`, `model2`) VALUES ('92919', '9', '196.291', '-2016.32', '142.777', '0', '0', '0', '0', '0', '0', '0', '0', '0', '100', '0', '0');
+INSERT INTO `creature_movement` (`id`, `point`, `position_x`, `position_y`, `position_z`, `waittime`, `script_id`, `textid1`, `textid2`, `textid3`, `textid4`, `textid5`, `emote`, `spell`, `orientation`, `model1`, `model2`) VALUES ('92919', '10', '198.622', '-2032.04', '137.491', '0', '0', '0', '0', '0', '0', '0', '0', '0', '100', '0', '0');
+INSERT INTO `creature_movement` (`id`, `point`, `position_x`, `position_y`, `position_z`, `waittime`, `script_id`, `textid1`, `textid2`, `textid3`, `textid4`, `textid5`, `emote`, `spell`, `orientation`, `model1`, `model2`) VALUES ('92919', '11', '207.454', '-2045.2', '133.071', '0', '0', '0', '0', '0', '0', '0', '0', '0', '100', '0', '0');
+INSERT INTO `creature_movement` (`id`, `point`, `position_x`, `position_y`, `position_z`, `waittime`, `script_id`, `textid1`, `textid2`, `textid3`, `textid4`, `textid5`, `emote`, `spell`, `orientation`, `model1`, `model2`) VALUES ('92919', '12', '224.699', '-2066.14', '127.224', '0', '0', '0', '0', '0', '0', '0', '0', '0', '100', '0', '0');
+INSERT INTO `creature_movement` (`id`, `point`, `position_x`, `position_y`, `position_z`, `waittime`, `script_id`, `textid1`, `textid2`, `textid3`, `textid4`, `textid5`, `emote`, `spell`, `orientation`, `model1`, `model2`) VALUES ('92919', '13', '235.483', '-2086.11', '122.593', '0', '0', '0', '0', '0', '0', '0', '0', '0', '100', '0', '0');
+INSERT INTO `creature_movement` (`id`, `point`, `position_x`, `position_y`, `position_z`, `waittime`, `script_id`, `textid1`, `textid2`, `textid3`, `textid4`, `textid5`, `emote`, `spell`, `orientation`, `model1`, `model2`) VALUES ('92919', '14', '233.908', '-2110.25', '117.959', '0', '0', '0', '0', '0', '0', '0', '0', '0', '100', '0', '0');
+INSERT INTO `creature_movement` (`id`, `point`, `position_x`, `position_y`, `position_z`, `waittime`, `script_id`, `textid1`, `textid2`, `textid3`, `textid4`, `textid5`, `emote`, `spell`, `orientation`, `model1`, `model2`) VALUES ('92919', '15', '230.995', '-2125.5', '117.604', '0', '0', '0', '0', '0', '0', '0', '0', '0', '100', '0', '0');
+INSERT INTO `creature_movement` (`id`, `point`, `position_x`, `position_y`, `position_z`, `waittime`, `script_id`, `textid1`, `textid2`, `textid3`, `textid4`, `textid5`, `emote`, `spell`, `orientation`, `model1`, `model2`) VALUES ('92919', '16', '233.087', '-2143.76', '119.295', '0', '0', '0', '0', '0', '0', '0', '0', '0', '100', '0', '0');
+INSERT INTO `creature_movement` (`id`, `point`, `position_x`, `position_y`, `position_z`, `waittime`, `script_id`, `textid1`, `textid2`, `textid3`, `textid4`, `textid5`, `emote`, `spell`, `orientation`, `model1`, `model2`) VALUES ('92919', '17', '233.113', '-2162.84', '120.015', '0', '0', '0', '0', '0', '0', '0', '0', '0', '100', '0', '0');
+INSERT INTO `creature_movement` (`id`, `point`, `position_x`, `position_y`, `position_z`, `waittime`, `script_id`, `textid1`, `textid2`, `textid3`, `textid4`, `textid5`, `emote`, `spell`, `orientation`, `model1`, `model2`) VALUES ('92919', '18', '238.967', '-2174.68', '118.833', '0', '0', '0', '0', '0', '0', '0', '0', '0', '100', '0', '0');
