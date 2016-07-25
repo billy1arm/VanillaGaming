1322495928eabf6bf26d2b362ec4aa23263fb414
xfury <xfury@foxmail.com>
2016-07-25 14:25:39 +0800

    [V-Data]修复任务-铸造奎尔塞拉
    道具-未淬火的上古之刃-在被黑龙MM吐息后会正确变成道具-上古淬火之剑

--- sql/update/mangos/spell_script_target.sql
+++ sql/update/mangos/spell_script_target.sql
@@ -12,6 +12,10 @@ File Encoding         : 65001
 -- Updates of spell_script_target
 -- ----------------------------
 
+-- Heated Blade DND
+DELETE FROM `spell_script_target` WHERE `entry`='22904';
+INSERT INTO `spell_script_target` (`entry`, `type`, `targetEntry`, `inverseEffectMask`) VALUES ('22904', '0', '179561', '0');
+
 -- 孵化蜘蛛卵
 DELETE FROM `spell_script_target` WHERE `entry`='24082';
 INSERT INTO `spell_script_target` (`entry`, `type`, `targetEntry`, `inverseEffectMask`) VALUES ('24082', '0', '179985', '0');
