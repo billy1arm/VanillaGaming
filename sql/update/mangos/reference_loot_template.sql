f60c43a8ea2c21d7095c4a9282d41224ce3a2e85
xfury <xfury@foxmail.com>
2016-08-18 03:06:05 +0800

    [V-Data]调整物品-提布的炽炎长剑
    从游戏内移除

--- sql/update/mangos/reference_loot_template.sql
+++ sql/update/mangos/reference_loot_template.sql
@@ -13,7 +13,8 @@ File Encoding         : 65001
 -- ----------------------------
 
 -- 提布的炽炎长剑
-UPDATE `reference_loot_template` SET `ChanceOrQuestChance`='0.01' WHERE (`entry`='24091') AND (`item`='1728');
+DELETE FROM `reference_loot_template` WHERE `item`='1728';
+-- INSERT INTO `reference_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES ('24091', '1728', '0', '1', '1', '1', '0');
 
 -- 汉娜之刃
 UPDATE `reference_loot_template` SET `ChanceOrQuestChance`='49.995' WHERE (`entry`='24091') AND (`item`='2801');
