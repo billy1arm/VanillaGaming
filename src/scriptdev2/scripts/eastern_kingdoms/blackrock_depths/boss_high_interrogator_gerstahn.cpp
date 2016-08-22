e252d257ed3a8ebd6f6c0eb9c2eb59dc99c98afc
xfury <xfury@foxmail.com>
2016-08-23 00:55:12 +0800

    [V-Core]修复黑石深渊-审讯官格斯塔恩
    标准化脚本AI

--- src/scriptdev2/scripts/eastern_kingdoms/blackrock_depths/boss_high_interrogator_gerstahn.cpp
+++ src/scriptdev2/scripts/eastern_kingdoms/blackrock_depths/boss_high_interrogator_gerstahn.cpp
@@ -1,4 +1,4 @@
-/* This file is part of the ScriptDev2 Project. See AUTHORS file for Copyright information
+﻿/* This file is part of the ScriptDev2 Project. See AUTHORS file for Copyright information
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
@@ -18,81 +18,80 @@
 SDName: Boss_High_Interrogator_Gerstahn
 SD%Complete: 100
 SDComment:
-SDCategory: Blackrock Depths
+SDCategory: 黑石深渊
 EndScriptData */
 
 #include "precompiled.h"
 
 enum
 {
-    SPELL_SHADOWWORDPAIN        = 14032,
-    SPELL_MANABURN              = 14033,
-    SPELL_PSYCHICSCREAM         = 13704,
-    SPELL_SHADOWSHIELD          = 12040
+    SPELL_SHADOWSHIELD          = 12040,                    // 暗影之盾
+    SPELL_PSYCHICSCREAM         = 13704,                    // 心灵尖啸
+    SPELL_SHADOWWORDPAIN        = 14032,                    // 暗言术:痛
+    SPELL_MANABURN              = 14033,                    // 法力燃烧
 };
 
 struct boss_high_interrogator_gerstahnAI : public ScriptedAI
 {
     boss_high_interrogator_gerstahnAI(Creature* pCreature) : ScriptedAI(pCreature) {Reset();}
 
+    uint32 m_uiShadowShieldTimer;
+    uint32 m_uiPsychicScreamTimer;
     uint32 m_uiShadowWordPainTimer;
     uint32 m_uiManaBurnTimer;
-    uint32 m_uiPsychicScreamTimer;
-    uint32 m_uiShadowShieldTimer;
 
     void Reset() override
     {
-        m_uiShadowWordPainTimer = 4000;
-        m_uiManaBurnTimer = 14000;
-        m_uiPsychicScreamTimer = 32000;
-        m_uiShadowShieldTimer = 8000;
+        m_uiShadowShieldTimer       = 8000;
+        m_uiPsychicScreamTimer      = 32000;
+        m_uiShadowWordPainTimer     = 4000;
+        m_uiManaBurnTimer           = 14000;
     }
 
     void UpdateAI(const uint32 uiDiff) override
     {
-        // Return since we have no target
         if (!m_creature->SelectHostileTarget() || !m_creature->getVictim())
-            return;
+            { return; }
 
-        // ShadowWordPain_Timer
-        if (m_uiShadowWordPainTimer < uiDiff)
-        {
-            if (Unit* pTarget = m_creature->SelectAttackingTarget(ATTACKING_TARGET_RANDOM, 0))
-                DoCastSpellIfCan(pTarget, SPELL_SHADOWWORDPAIN);
-
-            m_uiShadowWordPainTimer = 7000;
-        }
-        else
-            m_uiShadowWordPainTimer -= uiDiff;
-
-        // ManaBurn_Timer
-        if (m_uiManaBurnTimer < uiDiff)
-        {
-            if (Unit* pTarget = m_creature->SelectAttackingTarget(ATTACKING_TARGET_RANDOM, 0, SPELL_MANABURN, SELECT_FLAG_POWER_MANA))
-                DoCastSpellIfCan(pTarget, SPELL_MANABURN);
-
-            m_uiManaBurnTimer = 10000;
-        }
-        else
-            m_uiManaBurnTimer -= uiDiff;
-
-        // PsychicScream_Timer
-        if (m_uiPsychicScreamTimer < uiDiff)
-        {
-            DoCastSpellIfCan(m_creature, SPELL_PSYCHICSCREAM);
-            m_uiPsychicScreamTimer = 30000;
-        }
-        else
-            m_uiPsychicScreamTimer -= uiDiff;
-
-        // ShadowShield_Timer
+        // 暗影之盾
         if (m_uiShadowShieldTimer < uiDiff)
         {
             DoCastSpellIfCan(m_creature, SPELL_SHADOWSHIELD);
             m_uiShadowShieldTimer = 25000;
         }
         else
-            m_uiShadowShieldTimer -= uiDiff;
+            { m_uiShadowShieldTimer -= uiDiff; }
+
+        // 心灵尖啸
+        if (m_uiPsychicScreamTimer < uiDiff)
+        {
+            DoCastSpellIfCan(m_creature, SPELL_PSYCHICSCREAM);
+            m_uiPsychicScreamTimer = 30000;
+        }
+        else
+            { m_uiPsychicScreamTimer -= uiDiff; }
+
+        // 暗言术:痛
+        if (m_uiShadowWordPainTimer < uiDiff)
+        {
+            if (Unit* pTarget = m_creature->SelectAttackingTarget(ATTACKING_TARGET_RANDOM, 0))
+                { DoCastSpellIfCan(pTarget, SPELL_SHADOWWORDPAIN); }
+
+            m_uiShadowWordPainTimer = 7000;
+        }
+        else
+            { m_uiShadowWordPainTimer -= uiDiff; }
+
+        // 法力燃烧
+        if (m_uiManaBurnTimer < uiDiff)
+        {
+            if (Unit* pTarget = m_creature->SelectAttackingTarget(ATTACKING_TARGET_RANDOM, 0, SPELL_MANABURN, SELECT_FLAG_POWER_MANA))
+                { DoCastSpellIfCan(pTarget, SPELL_MANABURN); }
+
+            m_uiManaBurnTimer = 10000;
+        }
+        else
+            { m_uiManaBurnTimer -= uiDiff; }
 
         DoMeleeAttackIfReady();
     }
