c1b995e41332ca283c1d16ce41c92eb607f76b03
xfury <xfury@foxmail.com>
2016-08-29 11:47:38 +0800

    [V-Data]调整商人-兰拉
    移除黑莲花(开60后加入)

--- sql/update/mangos/npc_vendor.sql
+++ sql/update/mangos/npc_vendor.sql
@@ -16,6 +16,10 @@ File Encoding         : 65001
 DELETE FROM `npc_vendor` WHERE (`entry`='2366') AND (`item`='21151');
 -- INSERT INTO `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`, `condition_id`) VALUES ('2366', '21151', '0', '0', '0');
 
+-- 兰拉 --- 黑莲花
+DELETE FROM `npc_vendor` WHERE (`entry`='14846') AND (`item`='13468');
+-- INSERT INTO `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`, `condition_id`) VALUES ('14846', '13468', '1', '43200', '0');
+
 -- 维尔 --- 格罗姆之血
 UPDATE `npc_vendor` SET `maxcount`='3', `incrtime`='43200' WHERE (`entry`='16015') AND (`item`='8846');
 
