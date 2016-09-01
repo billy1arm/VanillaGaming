75433b7d558fde8f8d0e5be8a7d5e6c6ab1ea6b8
xfury <xfury@foxmail.com>
2016-09-01 16:26:39 +0800

    [V-Data]移除无用的数据库脚本AI喊话

---
+++ sql/update/mangos/creature_ai_texts.sql
@@ -0,0 +1,18 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : VanillaGaming
+Source Database       : z-mangos
+
+Target Server Type    : MYSQL
+File Encoding         : 65001
+*/
+
+-- ----------------------------
+-- Updates of creature_ai_texts
+-- ----------------------------
+
+-- 无用的数据库脚本AI喊话
+DELETE FROM `creature_ai_texts` WHERE `entry`='-212';
+DELETE FROM `creature_ai_texts` WHERE `entry`='-320';
+DELETE FROM `creature_ai_texts` WHERE `entry`='-1047';
