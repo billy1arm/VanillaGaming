bf1fa3a2b6cf018166c7e6457dc50e696f050df3
xfury <xfury@foxmail.com>
2016-09-01 16:44:33 +0800

    [V-Data]调整道具-次级血石矿脉
    刷新池

--- sql/update/mangos/pool_gameobject.sql
+++ sql/update/mangos/pool_gameobject.sql
@@ -12,6 +12,9 @@ File Encoding         : 65001
 -- Updates of pool_gameobject
 -- ----------------------------
 
+-- 次级血石矿脉
+DELETE FROM `pool_gameobject` WHERE `description`like'Lesser Bloodstone Deposit%';
+
 -- 黑莲花
 UPDATE `pool_gameobject` SET `description`='黑莲花-燃烧平原' WHERE (`description`='Burning Steppes - Black Lotus');
 UPDATE `pool_gameobject` SET `description`='黑莲花-东瘟疫之地' WHERE (`description`like'Black Lotus zone 139%');
