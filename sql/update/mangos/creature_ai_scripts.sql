13a91c6f29a101e0474869967692ccb61db3637c
xfury <xfury@foxmail.com>
2016-09-16 06:33:58 +0800

    [V-Data]修复生物-死灵勇士
    5%几率转变为永醒的艾希尔

--- sql/update/mangos/creature_ai_scripts.sql
+++ sql/update/mangos/creature_ai_scripts.sql
@@ -15,6 +15,10 @@ File Encoding         : 65001
 -- 奈塔拉什
 DELETE FROM `creature_ai_scripts` WHERE `id`='198302';
 
+-- 死灵勇士
+DELETE FROM `creature_ai_scripts` WHERE `id`='648804';
+INSERT INTO `creature_ai_scripts` (`id`, `creature_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_type`, `action1_param1`, `action1_param2`, `action1_param3`, `action2_type`, `action2_param1`, `action2_param2`, `action2_param3`, `action3_type`, `action3_param1`, `action3_param2`, `action3_param3`, `comment`) VALUES ('648804', '6488', '11', '0', '5', '0', '0', '0', '0', '0', '36', '6490', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '死灵勇士 - 转变为永醒的艾希尔');
+
 -- 碧火欺诈者
 UPDATE `creature_ai_scripts` SET `action1_param2`='5' WHERE (`id`='710703');
 
